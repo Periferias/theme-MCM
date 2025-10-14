06313fadab63f3b84a26ebf42c1ca892f63b9ca5
Victor Ferreira <vitfera@hotmail.com>
2025-10-13 23:34:42 -0300

    configuração de imagens da home

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php 
+use \MapasCulturais\i;
+
+return [
+    'app.siteName' => 'Prêmio Periferia Viva 2025',
+    /* 
+    Define o nome do asset da imagem da logo do site - Substituirá a logo padrão
+
+    ex: `img/meu-mapa-logo.jpg` (pasta assets/img/meu-mapa-logo.jpg do tema) 
+    */
+    'logo.image' => './img/logo-site.png',
+    'logo.hideLabel' => env('LOGO_HIDELABEL', true),
+
+    /* 
+    Define o nome do asset da imagem do background e banner no header da home - Substituirá o background padrão
+    ex: `img/meu-home-header-background.jpg` (pasta assets/img/meu-home-header-background.jpg do tema)
+    */
+    'homeHeader.background' => './img/home-header/home-header2.jpg',
+];
