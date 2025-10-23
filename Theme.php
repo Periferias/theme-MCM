1bb7c4cceca2982a62ba7f7735f0699e72768d5c
Victor Ferreira <vitfera@hotmail.com>
2025-10-23 16:24:41 -0300

    feat: Update MCM theme with new assets and styles
    - Added new images: minha-casa-melhor-banner.svg, regmel-logo.svg, home-header2.jpg
    - Updated theme-BaseV2.scss with unified display:none rules
    - Added compiled CSS and other assets
    - Updated views and components structure

--- Theme.php
+++ Theme.php
@@ -4,8 +4,9 @@ namespace MCM;
 
 use MapasCulturais\App;
 
-class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
-
+// class Theme extends \Subsite\Theme {
+class Theme extends \MapasCulturais\Themes\BaseV2\Theme
+{
     static function getThemeFolder()
     {
         return __DIR__;
@@ -16,36 +17,5 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
         parent::_init();
 
         $app = App::i();
-
-        // $app->hook('template(<<*>>.head):end', function () {
-        //     $this->part('google-analytics--script');
-        // });
-
-        // // Manifest do five icon
-        // $app->hook('GET(site.webmanifest)', function() use ($app) {
-        //     /** @var \MapasCulturais\Controller $this */
-        //     $this->json([
-        //         'icons' => [
-        //             [ 'src' => $app->view->asset('img/favicon-192x192.png', false), 'type' => 'image/png', 'sizes' => '192x192' ],
-        //             [ 'src' => $app->view->asset('img/favicon-512x512.png', false), 'type' => 'image/png', 'sizes' => '512x512' ]
-        //         ],
-        //     ]);
-        // });
-
-        // $app->hook('GET(agent.single)', function() use ($app) {
-        //   header('Location: https://interativo-mapadasperiferias.cidades.gov.br/nos-perifericos/meu-cadastro');
-        //   exit;
-        // });
-
-        // $app->hook('before.save:registration', function(Registration $registration) {
-
-        //     if (isset($registration->appliedPointReward['rules']) && is_array($registration->appliedPointReward['rules'])) {
-        //         foreach ($registration->appliedPointReward['rules'] as &$rule) {
-        //             if (!isset($rule['percentage'])) {
-        //                 $rule['percentage'] = 0;
-        //             }
-        //         }
-        //     }
-        // });
     }
 }
---
+++ views/site/index.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+$this->import('
+    home-entities 
+    home-feature
+    home-header 
+    home-map 
+    home-opportunities 
+    home-register
+');
+?>
+<home-header></home-header>
+<home-opportunities></home-opportunities>
